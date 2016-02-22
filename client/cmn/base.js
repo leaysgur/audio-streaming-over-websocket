86c70270e52e792c2fb138c249c0f9b8851050ef
leader22 <y.sugiura.0316@gmail.com>
2016-02-22 14:13:55 +0900

    socket_server

--- client/cmn/base.js
+++ client/cmn/base.js
@@ -3,5 +3,6 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext || window
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
 window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame;
 
-window.SOCKET_SERVER = 'http://192.168.10.24:3000';
+// window.SOCKET_SERVER = 'http://192.168.10.24:3000';
+window.SOCKET_SERVER = 'http://dev.lealog.net:9999/sock/';
 window.BUFFER_SIZE = 1024;
