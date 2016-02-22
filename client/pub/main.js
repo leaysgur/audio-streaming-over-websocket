408e5d94a33632a4540dab3993afe8322f17e9e7
leader22 <y.sugiura.0316@gmail.com>
2016-02-22 09:38:32 +0900

    refactor

--- client/pub/main.js
+++ client/pub/main.js
@@ -33,24 +33,9 @@ var pubApp = {
     startRec: function() {
       if (!this.stream) { return; }
       if (this.audio.source && this.audio.processor) { return; }
-      var socket = this.socket;
       this.audio.source    = this.ctx.createMediaStreamSource(this.stream);
       this.audio.processor = this.ctx.createScriptProcessor(1024);
-      this.audio.processor.onaudioprocess = function(ev) {
-        var inputBuffer  = ev.inputBuffer;
-        var outputBuffer = ev.outputBuffer;
-        var buffers = [];
-        for (var ch = 0; ch < outputBuffer.numberOfChannels; ch++) {
-          var inputData  = inputBuffer.getChannelData(ch);
-          var outputData = outputBuffer.getChannelData(ch);
-          buffers[ch] = new Float32Array(1024);
-
-          for (var sample = 0; sample < inputBuffer.length; sample++) {
-            outputData[sample] = buffers[ch][sample] = inputData[sample];
-          }
-        }
-        socket.emit('audio', buffers[0].buffer);
-      };
+      this.audio.processor.onaudioprocess = this._onAudioProcess;
       this.audio.source.connect(this.audio.processor);
       this.audio.processor.connect(this.ctx.destination);
     },
@@ -59,6 +44,22 @@ var pubApp = {
       this.audio.source    && this.audio.source.disconnect();
       this.audio.processor = null;
       this.audio.source    = null;
+    },
+    _onAudioProcess: function(ev) {
+      var socket = this.socket;
+      var inputBuffer  = ev.inputBuffer;
+      var outputBuffer = ev.outputBuffer;
+      var buffers = [];
+      for (var ch = 0; ch < outputBuffer.numberOfChannels; ch++) {
+        var inputData  = inputBuffer.getChannelData(ch);
+        var outputData = outputBuffer.getChannelData(ch);
+        buffers[ch] = new Float32Array(1024);
+
+        for (var sample = 0; sample < inputBuffer.length; sample++) {
+          outputData[sample] = buffers[ch][sample] = inputData[sample];
+        }
+      }
+      socket.emit('audio', buffers[0].buffer);
     },
     _hookCreated: function() {
       var $data = this.$data;
