17d2e6978da21d17d1b85bd74e9221e1eec2014f
leader22 <y.sugiura.0316@gmail.com>
2016-02-22 10:54:38 +0900

    add analyser

--- client/cmn/base.js
+++ client/cmn/base.js
@@ -1,4 +1,6 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
+window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame;
 
 window.SOCKET_SERVER = 'http://192.168.10.24:3000';
--- client/pub/main.js
+++ client/pub/main.js
@@ -15,7 +15,12 @@ var pubApp = {
     socket: null,
     stream: null,
     ctx:    null,
-    audio:  { source: null, processor: null },
+    audio:  {
+      source:    null,
+      processor: null,
+      analyser:  null,
+      gain:      null
+    },
     subNum: 0
   },
   methods: {
@@ -34,16 +39,32 @@ var pubApp = {
       if (!this.stream) { return; }
       if (this.audio.source && this.audio.processor) { return; }
       this.audio.source    = this.ctx.createMediaStreamSource(this.stream);
+      this.audio.analyser  = this.ctx.createAnalyser();
       this.audio.processor = this.ctx.createScriptProcessor(1024);
+      this.audio.gain      = this.ctx.createGain();
+
+      this.audio.analyser.smoothingTimeConstant = 0.4;
+      this.audio.analyser.fftSize = 1024;
       this.audio.processor.onaudioprocess = this._onAudioProcess;
+      this.audio.gain.gain.value = 0;
+
       this.audio.source.connect(this.audio.processor);
-      this.audio.processor.connect(this.ctx.destination);
+      this.audio.source.connect(this.audio.analyser);
+      this.audio.processor.connect(this.audio.gain);
+      this.audio.gain.connect(this.ctx.destination);
+
+      this._drawInputSpectrum();
     },
     stopRec: function() {
+      cancelAnimationFrame(this._drawInputSpectrum);
       this.audio.processor && this.audio.processor.disconnect();
       this.audio.source    && this.audio.source.disconnect();
+      this.audio.analyser  && this.audio.analyser.disconnect();
+      this.audio.gain      && this.audio.gain.disconnect();
       this.audio.processor = null;
       this.audio.source    = null;
+      this.audio.analyser  = null;
+      this.audio.gain      = null;
     },
     _onAudioProcess: function(ev) {
       var socket = this.socket;
@@ -61,6 +82,25 @@ var pubApp = {
       }
       socket.emit('audio', buffers[0].buffer);
     },
+    _drawInputSpectrum: function() {
+      if (!this.audio.analyser) { return; }
+      var $canvas = this.$els.canvas;
+      var drawContext = $canvas.getContext('2d');
+      var fbc = this.audio.analyser.frequencyBinCount;
+      var freqs = new Uint8Array(this.audio.analyser.frequencyBinCount);
+      this.audio.analyser.getByteFrequencyData(freqs);
+
+      drawContext.clearRect(0, 0, $canvas.width, $canvas.height);
+      for (var i = 0; i < freqs.length; i++) {
+        var barWidth = $canvas.width / fbc;
+        // 0 - 255の値が返るのでそれを使って描画するバーの高さを得る
+        var height = $canvas.height * (freqs[i] / 255);
+        var offset = $canvas.height - height;
+        drawContext.fillStyle = 'hsl(' + (i / fbc * 360) + ', 100%, 50%)';
+        drawContext.fillRect(i * barWidth, offset, barWidth + 1, height);
+      }
+      requestAnimationFrame(this._drawInputSpectrum);
+    },
     _hookCreated: function() {
       var $data = this.$data;
       this.ctx = new AudioContext();
@@ -68,10 +108,17 @@ var pubApp = {
       this.socket.on('subNum', function(num) {
         $data.subNum = num;
       });
+    },
+    _hookAttached: function() {
+      this.$els.canvas.width  = window.innerWidth;
+      this.$els.canvas.height = window.innerWidth / 10;
+      this.$els.canvas.style.width  = '100%';
+      this.$els.canvas.style.height = '10%';
     }
   },
   events: {
-    'hook:created': function() { this._hookCreated(); }
+    'hook:created':  function() { this._hookCreated(); },
+    'hook:attached': function() { this._hookAttached(); }
   }
 };
 
